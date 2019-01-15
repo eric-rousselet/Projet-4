e8d87d154480e6b2deac1be70a8d17133650368e
Eric Rousselet <rousselet.eric@gmail.com>
2019-01-15 16:31:39 +0100

    entity and crud

---
+++ src/Entity/Movie.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\MovieRepository")
+ */
+class Movie
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=1000)
+     */
+    private $title;
+
+    /**
+     * @ORM\Column(type="string", length=10000)
+     */
+    private $synopsis;
+
+    /**
+     * @ORM\Column(type="string", length=500, nullable=true)
+     */
+    private $picture;
+
+    /**
+     * @ORM\Column(type="integer", nullable=true)
+     */
+    private $release_date;
+
+    /**
+     * @ORM\Column(type="string", length=500)
+     */
+    private $director;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getSynopsis(): ?string
+    {
+        return $this->synopsis;
+    }
+
+    public function setSynopsis(string $synopsis): self
+    {
+        $this->synopsis = $synopsis;
+
+        return $this;
+    }
+
+    public function getPicture(): ?string
+    {
+        return $this->picture;
+    }
+
+    public function setPicture(?string $picture): self
+    {
+        $this->picture = $picture;
+
+        return $this;
+    }
+
+    public function getReleaseDate(): ?int
+    {
+        return $this->release_date;
+    }
+
+    public function setReleaseDate(?int $release_date): self
+    {
+        $this->release_date = $release_date;
+
+        return $this;
+    }
+
+    public function getDirector(): ?string
+    {
+        return $this->director;
+    }
+
+    public function setDirector(string $director): self
+    {
+        $this->director = $director;
+
+        return $this;
+    }
+}
---
+++ src/Migrations/Version20190115152644.php
@@ -0,0 +1,28 @@
+<?php declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20190115152644 extends AbstractMigration
+{
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE movie CHANGE title title VARCHAR(1000) NOT NULL, CHANGE synopsis synopsis VARCHAR(10000) NOT NULL');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE movie CHANGE title title VARCHAR(255) NOT NULL COLLATE utf8mb4_unicode_ci, CHANGE synopsis synopsis VARCHAR(2000) NOT NULL COLLATE utf8mb4_unicode_ci');
+    }
+}
