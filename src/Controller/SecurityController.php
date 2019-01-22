48374ea5f051970096137d4dcfcd1d56e7c3cb8b
Eric Rousselet <rousselet.eric@gmail.com>
2019-01-22 16:02:07 +0100

    register

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,13 +2,20 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\UserType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
 {
+    const DEFAULT_ROLE=["ROLE_MEMBER"];
+
     /**
      * @Route("/login", name="app_login")
      */
@@ -20,5 +27,29 @@ class SecurityController extends AbstractController
         $lastUsername = $authenticationUtils->getLastUsername();
 
         return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+    }
+
+    /**
+     * @Route("/register", name="app_register")
+     */
+    public function register(Request $request, EntityManagerInterface $em, UserPasswordEncoderInterface $passwordEncoder): Response
+    {
+        $user=new User();
+        $form=$this->createForm(UserType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $user->setPassword(
+                $passwordEncoder->encodePassword(
+                    $user,
+                    $user->getPassword()
+                )
+            );
+            $user->setRoles(self::DEFAULT_ROLE);
+            $em->persist($user);
+            $em->flush();
+        }
+
+        return $this->render('security/register.html.twig', ['form'=>$form->createView()]);
     }
 }
--- src/Security/AppAuthenticator.php
+++ src/Security/AppAuthenticator.php
@@ -85,7 +85,7 @@ class AppAuthenticator extends AbstractFormLoginAuthenticator
             return new RedirectResponse($targetPath);
         }
 
-        return new RedirectResponse($this->router->generate('article_new'));
+        return new RedirectResponse($this->router->generate('home'));
     }
 
     protected function getLoginUrl()
